15130c5d7b3ff0c501481d74f916f586c537393c
joema <joema@DESKTOP-FNRCPCH>
2023-06-23 17:15:40 -0230

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This is your mod's main class.
+	/// </summary>
+
+	/* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+	[ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public GameObject cube; 
+		bool inRoom;
+
+		void Start() 
+		{
+			/* A lot of Gorilla Tag systems will not be set up when start is called /*
+			/* Put code in OnGameInitialized to avoid null references */
+
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnEnable()
+		{
+			/* Set up your mod here */
+			/* Code here runs at the start and whenever your mod is enabled*/
+
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			/* Undo mod setup here */
+			/* This provides support for toggling mods with ComputerInterface, please implement it :) */
+			/* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+			/* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+			// creates cube
+			cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			// disable collider
+			cube.GetComponent<BoxCollider>().enabled = false;
+			// change size
+			cube.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
+			// change the floats to change cube position
+			cube.transform.localPosition = new Vector3(-0.055f, 0.0004f, -0.01f);
+			// set the parent of the cube to the hand
+			cube.transform.SetParent(GorillaLocomotion.Player.Instance.rightHandFollower.transform, false);
+		}
+
+		void Update()
+		{
+			/* Code here runs every frame when the mod is enabled */
+			// changes color
+			cube.GetComponent<MeshRenderer>().material.color = Color.blue;
+		}
+
+		/* This attribute tells Utilla to call this method when a modded room is joined */
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			/* Activate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+			
+
+			inRoom = true;
+			
+		}
+
+		/* This attribute tells Utilla to call this method when a modded room is left */
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+			/* Deactivate your mod here */
+			/* This code will run regardless of if the mod is enabled*/
+
+			inRoom = false;
+		}
+	}
+}
